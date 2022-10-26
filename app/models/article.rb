02eb08c1f124f6b79430ce699844b8331e4fadc5
Rachel Graves <rachel@didlix.co.uk>
2022-10-26 09:47:19 +0100

    Add id to article model
    I am adding the ability to record likes against articles. To do this I need a unique id for the article which I will record a ArticleLike record against.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Article
-  attr_reader :title
+  attr_reader :title, :id
 
   def initialize(attributes)
     @title = attributes[:title]
+    @id = attributes[:id]
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,11 +7,15 @@ describe Article do
     subject(:article) { described_class.new(assigned_attributes) }
 
     let(:assigned_attributes) do
-      { title: 'Three little birds' }
+      { id: 42, title: 'Three little birds' }
     end
 
     it 'assigns the title from a hash of attributes' do
       expect(article.title).to eq(assigned_attributes[:title])
+    end
+
+    it 'assigns the id from a hash of attributes' do
+      expect(article.id).to eq(assigned_attributes[:id])
     end
   end
 end
