a88fed6734bfa984d4d690c6f5f1dfb19bbda43d
Rachel Graves <rachel@didlix.co.uk>
2022-10-26 11:02:29 +0100

    Refactor article_spec
    While editing this file I noticed I'd nested the spec for the `id` method within thedescriptor block for the `title` method.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,17 +3,19 @@
 require 'rails_helper'
 
 describe Article do
+  subject(:article) { described_class.new(assigned_attributes) }
+
+  let(:assigned_attributes) do
+    { id: 42, title: 'Three little birds' }
+  end
+
   describe '#title' do
-    subject(:article) { described_class.new(assigned_attributes) }
-
-    let(:assigned_attributes) do
-      { id: 42, title: 'Three little birds' }
-    end
-
     it 'assigns the title from a hash of attributes' do
       expect(article.title).to eq(assigned_attributes[:title])
     end
+  end
 
+  describe '#id' do
     it 'assigns the id from a hash of attributes' do
       expect(article.id).to eq(assigned_attributes[:id])
     end
