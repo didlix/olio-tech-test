e057654546d921100a55573c111174f8516d75f9
Rachel Graves <rachel@didlix.co.uk>
2022-10-26 12:56:15 +0100

    Add ArticlesController
    Not that the ArticlesService and Article 'model' are in place I have
    created the ArticlesController. Its job is to use the ArticlesService
    to fetch articles from the hosted json file, and then assign them to
    the view.
    
    I've included some basic styling for the ArticlesController#index page
    which I'll expand on in future commits.
    
    In future commits we'd want to handle what happens when:
    - No articles are returned, i,e, display a friendly message
    - An error is returned by the ArticlesService

---
+++ spec/features/articles_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Articles', type: :feature do
+  describe 'Viewing Articles' do
+    subject(:visit_articles_page) do
+      visit(articles_path)
+    end
+
+    let(:existing_articles) do
+      [
+        instance_double('Article', title: 'hello world'),
+        instance_double('Article', title: 'hello sky')
+      ]
+    end
+
+    it 'displays all articles on the articles page' do
+      allow(ArticlesService).to receive(:all).and_return(existing_articles)
+
+      visit_articles_page
+
+      expect(page).to have_content('hello world')
+    end
+  end
+end
